b1006e89b818e81df25a3686ffa7b44a9d7cefd4
AndreyWork <1yamzin98@mail.ru>
2023-05-24 13:08:17 +0700

    слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,6 @@
-new Swiper('.swiper', {
 
-    navigation:{
-      nextEl: '.slider-portfolio__next',
-      prevEl: '.slider-portfolio__prev'
-    },
-    spaceBetween:30,
-    slidesPerView: 4,
-    loop: true,
-    loopedSlides:1,
-
-});
+let prevButton = document.querySelector('.slider-portfolio__prev');
+let nextButton = document.querySelector('.slider-portfolio__next');
+let wrapperSlide = document.querySelector('.slider-portfolio-items-wrapper');
+let sliderContent = document.querySelector('.slider-portfolio-items');
+let sliderItem = document.querySelectorAll('.slider-portfolio__item');
