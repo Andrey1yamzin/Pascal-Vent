8589438c484d9b278b8b52c66f7a581dcb785225
ANDREY <1yamzin98@mail.ru>
2023-06-22 23:13:29 +0700

    create slider

--- js/slider2.js
+++ js/slider2.js
@@ -1,82 +1,77 @@
     
     let position2 = 0;
-    const slidesToShow2 = 1;
-    const slidesToScroll2 = 1;
-    const container2 = document.querySelector('.clients--slider-wrapper');
-    const track2 = document.querySelector('.clients--slider-items');
-    const items2 = document.querySelectorAll('.cleints-sleder_item');
-    const itemsCount2 = items2.length;
-    const nextButton2 = document.querySelector('.clients--slider__next');
-    const prevButton2 = document.querySelector('.clients--slider__prev');
-    const widthFocusItem2 = 500;
+    const slidesClientsToShow = 3;
+    const slidesClientsToScroll = 1;
+    const containerSlidesClients = document.querySelector('.clients--slider-content');
+    const trackSlidesClients = document.querySelector('.clients--slider-items');
+    const itemsSlidesClients = document.querySelectorAll('.cleints-sleder_item');
+    const itemsCountSlidesClients = itemsSlidesClients.length;
+    const nextButtonSlidesClients = document.querySelector('.clients--slider__next');
+    const prevButtonSlidesClients = document.querySelector('.clients--slider__prev');
+    const widthFocusItemSlidesClients = 500;
     //ширина каждого элемента
-    const itemWidth2 = 250 / slidesToShow2;
+    const itemWidthSlidesClients = containerSlidesClients.clientWidth / slidesClientsToShow;
     // 
-    const movePosition2 = slidesToScroll2 * itemWidth2;
-    let itemFocus2 = 0;
+    const movePositionSlidesClients = slidesClientsToScroll * itemWidthSlidesClients;
+
+
+    itemsSlidesClients.forEach((item2)=>{
+        // задаём ширину каждому элементу в css
+        item2.style.minWidth = `${itemWidthSlidesClients}px`;
+        
+    });
+
+
+    checkBtns2();
+    
+    nextButtonSlidesClients .addEventListener('click', () => {
+        const itemsLeft = itemsCountSlidesClients -(Math.abs(position2) + slidesClientsToShow * itemWidthSlidesClients) / itemWidthSlidesClients;
+        position2 -= itemsLeft >= slidesClientsToScroll ? movePositionSlidesClients : itemsLeft * itemWidthSlidesClients;
+       
+       
+
+        setPosition2();
+        checkBtns2();
+        
+    });
+    prevButtonSlidesClients.addEventListener('click', () => {
+        const itemsLeft = Math.abs(position2) / itemWidthSlidesClients;
+        position2 += itemsLeft >= slidesClientsToScroll ? movePositionSlidesClients : itemsLeft * itemWidthSlidesClients;
+        
+
+        setPosition2();
+        checkBtns2();
+        
+    });
     
 
-    items2.forEach((item2)=>{
-        // задаём ширину каждому элементу в css
-        item2.style.minWidth = `${itemWidth2}px`;
-        
-    });
-    checkBtns2();
-    addStyle2(getItemFocus2(itemFocus2));
-    
-    nextButton2.addEventListener('click', () => {
-        const itemsLeft = itemsCount2 -(Math.abs(position2) + slidesToShow2 * itemWidth2) / itemWidth2;
-        position2 -= itemsLeft >= slidesToScroll2 ? movePosition2 : itemsLeft * itemWidth2;
-        itemFocus2 += 1;
-        if(itemFocus2 > itemsCount2 - 1){
-            itemFocus2 = itemsCount2 - 1;
-        }else if(itemFocus2 < itemsCount2){
-            addStyle(getItemFocus2(itemFocus2));
-            removeStyle(getItemFocus2(itemFocus2 - 1))
-        }
-        setPosition2();
-        checkBtns2();
-        
-    });
-    prevButton2.addEventListener('click', () => {
-        const itemsLeft = Math.abs(position2) / itemWidth2;
-        position2 += itemsLeft >= slidesToScroll2 ? movePosition2 : itemsLeft * itemWidth2;
-        itemFocus2 -= 1;
-        if(itemFocus2 < 0){
-            itemFocus2 = 0;
-        }else if(itemFocus2 == itemsCount2 - 1 ||  itemFocus2 < itemsCount2 - 1 || itemFocus2 == 0){
-            addStyle2(getItemFocus2(itemFocus2));
-            removeStyle2(getItemFocus2(itemFocus2 + 1))
-        }
-        setPosition2();
-        checkBtns2();
-        
-    });
-    
+
+
+
     
     const setPosition2 = () =>{
         // смещение трека
-        track2.style.transform = `translateX(${position2}px)`;
+        trackSlidesClients.style.transform = `translateX(${position2}px)`;
     };
     function checkBtns2(){
-        prevButton2.disabled = position2 === 0;
-        nextButton2.disabled = position2 <= -(itemsCount2 - slidesToShow2) * itemWidth2;
+        prevButtonSlidesClients.disabled = position2 === 0;
+        nextButtonSlidesClients.disabled = position2 <= -(itemWidthSlidesClients - slidesClientsToShow) * itemWidthSlidesClients;
 
-        if(prevButton2.disabled === true){
-            prevButton2.classList.add('nonactive');
-            prevButton2.classList.remove('active');
+        if(prevButtonSlidesClients.disabled === true){
+            prevButtonSlidesClients.classList.add('nonactive');
+            prevButtonSlidesClients.classList.remove('active');
         }
-        if(nextButton2.disabled === true){
-            nextButton2.classList.add('nonactive');
-            nextButton2.classList.remove('active');
+        if(nextButtonSlidesClients.disabled === true){
+            nextButtonSlidesClients.classList.add('nonactive');
+            nextButtonSlidesClients.classList.remove('active');
         }
-        if(prevButton2.disabled === false){
-            prevButton2.classList.add('active');
-            prevButton2.classList.remove('nonactive');
+        if(prevButtonSlidesClients.disabled === false){
+            prevButtonSlidesClients.classList.add('active');
+            prevButtonSlidesClients.classList.remove('nonactive');
         }
-        if(nextButton2.disabled === false){
-            nextButton2.classList.add('active');
-            nextButton2.classList.remove('nonactive');
+        if(nextButtonSlidesClients.disabled === false){
+            nextButtonSlidesClients.classList.add('active');
+            nextButtonSlidesClients.classList.remove('nonactive');
         }
         setTimeout(checkBtns2, 1000);
     }
