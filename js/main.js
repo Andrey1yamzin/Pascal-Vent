1d333502fc8703687f7fa4f28e50ee71fa9e62a2
ANDREY <1yamzin98@mail.ru>
2023-07-05 21:41:31 +0700

    create adaptive

--- js/main.js
+++ js/main.js
@@ -1,83 +1,148 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict'
     
-    // let wrapBtn = document.querySelector('.tabs-button');
-    // let btn = document.querySelectorAll('.tabs-button__item');
-    // let tabs = document.querySelectorAll('.tabs-content');
-    // let header = document.querySelector('.header');
-    // let windowScroll;
-    // let windowWidth;
+    let wrapBtn = document.querySelector('.tabs-button');
+    let btn = document.querySelectorAll('.tabs-button__item');
+    let tabs = document.querySelectorAll('.tabs-content');
+    let header = document.querySelector('.header');
+    let windowScroll;
+    let windowWidth;
 
 
     let burgerBtn = document.querySelector('.header-burger__button');
     let burgerMenu = document.querySelector('.header-menu__burger');
 
-     
-    // function hideTabContent(hide){
-    //     for(let i = hide; i < tabs.length; i++){
-    //         tabs[i].classList.remove('show');
-    //         tabs[i].classList.add('hide');
-    //         btn[i].classList.remove('active');
-    //         if(btn[i].innerHTML == 'Система Вентиляции'){
-    //             btn[i].classList.add('non-active__ventilation');
-    //         }if(btn[i].innerHTML == 'Кондиционирование'){
-    //             btn[i].classList.remove('active__condition');
-    //             btn[i].classList.add('non-active');
-    //         }
+
+
+    
+
+let buttonSubmitAplication = document.querySelector('.subtitle-button');
+let showFormSubmitAplication = document.querySelector('.window--application');
+let closedFormSubmitAplication = document.querySelector('.window--application-button');
+let showFormSubmitAplication2 = document.querySelector('.application-content__button');
+
+let formContactUs = document.querySelector('.window-contentUs');
+let buttonShowFormContactUs = document.querySelector('.header-contact');
+let closedFormContactUs = document.querySelector('.window--contactUs-button');
+
+
+
+
+    function hideTabContent(hide){
+        for(let i = hide; i < tabs.length; i++){
+            tabs[i].classList.remove('show');
+            tabs[i].classList.add('hide');
+            btn[i].classList.remove('active');
+            if(btn[i].innerHTML == 'Система Вентиляции'){
+                btn[i].classList.add('non-active__ventilation');
+            }if(btn[i].innerHTML == 'Кондиционирование'){
+                btn[i].classList.remove('active__condition');
+                btn[i].classList.add('non-active');
+            }
            
-    //     }}
+        }}
     
-    //     hideTabContent(1);
-    
-    
-    // function showTabContent(show){
-    //     if(tabs[show].classList.contains('hide')){
-    //         tabs[show].classList.remove('hide');
-    //         tabs[show].classList.add('show');
-    //         btn[show].classList.remove('non-active');
-    //         if(btn[show].innerHTML == 'Система Вентиляции'){
-    //             btn[show].classList.remove('non-active__ventilation');
-    //             btn[show].classList.add('active');
-    //         }if(btn[show].innerHTML == 'Кондиционирование'){
-    //             btn[show].classList.remove('non-active');
-    //             btn[show].classList.add('active__condition');
-    //         }
-    //     }}
+        hideTabContent(1);
     
     
-    // wrapBtn.addEventListener('click', function(event){
-    //         let target = event.target;
-    //         if(target && target.classList.contains('tabs-button__item')){
-    //             for(let i = 0; i < btn.length; i++){
-    //                 if(target == btn[i]){
-    //                     hideTabContent(0);
-    //                     showTabContent(i);
-    //                     break; 
-    //                 }
-    //             }
-    //         }
-    //     })
+    function showTabContent(show){
+        if(tabs[show].classList.contains('hide')){
+            tabs[show].classList.remove('hide');
+            tabs[show].classList.add('show');
+            btn[show].classList.remove('non-active');
+            if(btn[show].innerHTML == 'Система Вентиляции'){
+                btn[show].classList.remove('non-active__ventilation');
+                btn[show].classList.add('active');
+            }if(btn[show].innerHTML == 'Кондиционирование'){
+                btn[show].classList.remove('non-active');
+                btn[show].classList.add('active__condition');
+            }
+        }}
+    
+    
+    wrapBtn.addEventListener('click', function(event){
+            let target = event.target;
+            if(target && target.classList.contains('tabs-button__item')){
+                for(let i = 0; i < btn.length; i++){
+                    if(target == btn[i]){
+                        hideTabContent(0);
+                        showTabContent(i);
+                        break; 
+                    }
+                }
+            }
+        })
 
 
 burgerBtn.addEventListener('click', function(){
     burgerMenu.classList.toggle('burger-activate');
 })
         
-// window.addEventListener('scroll', function(){
-//     windowScroll = window.scrollY; 
-//     windowWidth = window.innerWidth; 
-//     if(windowScroll >= 2){
-//         header.classList.add('header_fixed');
-//         console.log(windowWidth);
-//     }
-//     if(windowScroll <= 10 || windowWidth <= 1036){
-//         header.classList.remove('header_fixed');
+window.addEventListener('scroll', function(){
+    windowScroll = window.scrollY; 
+    windowWidth = window.innerWidth; 
+    if(windowScroll >= 2){
+        header.classList.add('header_fixed');
+        console.log(windowWidth);
+    }
+    if(windowScroll <= 10 || windowWidth <= 1036){
+        header.classList.remove('header_fixed');
         
-//     }
-//     if(windowWidth <= 1036){
-//         header.style.position = 'relative';
-//     }
-// });
+    }
+    if(windowWidth <= 1036){
+        header.style.position = 'relative';
+    }
+});
+
+
+
+
+
+
+
+
+buttonShowFormContactUs.addEventListener('click', function(){
+    formContactUs.classList.remove('hideWindowForm');
+    formContactUs.classList.add('showWindowForm');
+    document.body.style.overflow = 'hidden';
+
+    console.log('1')
+})
+
+closedFormContactUs.addEventListener('click', function(){
+    formContactUs.classList.remove('showWindowForm');
+    formContactUs.classList.add('hideWindowForm');
+    document.body.style.overflow = 'visible';
+})
+
+
+
+
+buttonSubmitAplication.addEventListener('click', function(){
+    showFormSubmitAplication.classList.remove('hideWindowForm');
+    showFormSubmitAplication.classList.add('showWindowForm');
+    document.body.style.overflow = 'hidden';
+    window.scrollTo(0,0);
+})
+
+closedFormSubmitAplication.addEventListener('click', function(){
+    showFormSubmitAplication.classList.remove('showWindowForm');
+    showFormSubmitAplication.classList.add('hideWindowForm');
+    document.body.style.overflow = 'visible';
+})
+
+
+showFormSubmitAplication2.addEventListener('click', function(){
+    showFormSubmitAplication.classList.remove('hideWindowForm');
+    showFormSubmitAplication.classList.add('showWindowForm');
+    document.body.style.overflow = 'hidden';
+});
+
+
+
+
+
+
     });
 
 //вынести в отельную функцию, для того что бы при загрузки ниже стр проверялось сколько px
--- js/slider.js
+++ js/slider.js
@@ -1,99 +1,99 @@
 
-    // let position = 0;
-    // const slidesToShow = 1;
-    // const slidesToScroll = 1;
-    // const container = document.querySelector('.portfolio-content');
-    // const track = document.querySelector('.slider-portfolio-items');
-    // const items = document.querySelectorAll('.slider-portfolio__item-body');
-    // const itemsCount = items.length;
-    // const nextButton = document.querySelector('.slider-portfolio__next');
-    // const prevButton = document.querySelector('.slider-portfolio__prev');
-    // const widthFocusItem = 500;
-    // //ширина каждого элемента
-    // const itemWidth = 270 / slidesToShow;
-    // // 
-    // const movePosition = slidesToScroll * itemWidth;
-    // let itemFocus = 0;
+    let position = 0;
+    const slidesToShow = 1;
+    const slidesToScroll = 1;
+    const container = document.querySelector('.portfolio-content');
+    const track = document.querySelector('.slider-portfolio-items');
+    const items = document.querySelectorAll('.slider-portfolio__item-body');
+    const itemsCount = items.length;
+    const nextButton = document.querySelector('.slider-portfolio__next');
+    const prevButton = document.querySelector('.slider-portfolio__prev');
+    const widthFocusItem = 500;
+    //ширина каждого элемента
+    const itemWidth = 270 / slidesToShow;
+    // 
+    const movePosition = slidesToScroll * itemWidth;
+    let itemFocus = 0;
     
 
-    // items.forEach((item)=>{
-    //     // задаём ширину каждому элементу в css
-    //     item.style.minWidth = `${itemWidth}px`;
+    items.forEach((item)=>{
+        // задаём ширину каждому элементу в css
+        item.style.minWidth = `${itemWidth}px`;
         
-    // });
-    // checkBtns();
-    // addStyle(getItemFocus(itemFocus));
+    });
+    checkBtns();
+    addStyle(getItemFocus(itemFocus));
     
-    // nextButton.addEventListener('click', () => {
-    //     const itemsLeft = itemsCount -(Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
-    //     position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-    //     itemFocus += 1;
-    //     if(itemFocus > itemsCount - 1){
-    //         itemFocus = itemsCount - 1;
-    //     }else if(itemFocus < itemsCount){
-    //         addStyle(getItemFocus(itemFocus));
-    //         removeStyle(getItemFocus(itemFocus - 1))
-    //     }
-    //     setPosition();
-    //     checkBtns();
+    nextButton.addEventListener('click', () => {
+        const itemsLeft = itemsCount -(Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+        position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+        itemFocus += 1;
+        if(itemFocus > itemsCount - 1){
+            itemFocus = itemsCount - 1;
+        }else if(itemFocus < itemsCount){
+            addStyle(getItemFocus(itemFocus));
+            removeStyle(getItemFocus(itemFocus - 1))
+        }
+        setPosition();
+        checkBtns();
         
-    // });
-    // prevButton.addEventListener('click', () => {
-    //     const itemsLeft = Math.abs(position) / itemWidth;
-    //     position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-    //     itemFocus -= 1;
-    //     if(itemFocus < 0){
-    //         itemFocus = 0;
-    //     }else if(itemFocus == itemsCount - 1 ||  itemFocus < itemsCount - 1 || itemFocus == 0){
-    //         addStyle(getItemFocus(itemFocus));
-    //         removeStyle(getItemFocus(itemFocus + 1))
-    //     }
-    //     setPosition();
-    //     checkBtns();
+    });
+    prevButton.addEventListener('click', () => {
+        const itemsLeft = Math.abs(position) / itemWidth;
+        position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+        itemFocus -= 1;
+        if(itemFocus < 0){
+            itemFocus = 0;
+        }else if(itemFocus == itemsCount - 1 ||  itemFocus < itemsCount - 1 || itemFocus == 0){
+            addStyle(getItemFocus(itemFocus));
+            removeStyle(getItemFocus(itemFocus + 1))
+        }
+        setPosition();
+        checkBtns();
         
-    // });
+    });
     
     
-    // const setPosition = () =>{
-    //     // смещение трека
-    //     track.style.transform = `translateX(${position}px)`;
-    // };
-    // function checkBtns(){
-    //     prevButton.disabled = position === 0;
-    //     nextButton.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+    const setPosition = () =>{
+        // смещение трека
+        track.style.transform = `translateX(${position}px)`;
+    };
+    function checkBtns(){
+        prevButton.disabled = position === 0;
+        nextButton.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
 
-    //     if(prevButton.disabled === true){
-    //         prevButton.classList.add('nonactive');
-    //         prevButton.classList.remove('active');
-    //     }
-    //     if(nextButton.disabled === true){
-    //         nextButton.classList.add('nonactive');
-    //         nextButton.classList.remove('active');
-    //     }
-    //     if(prevButton.disabled === false){
-    //         prevButton.classList.add('active');
-    //         prevButton.classList.remove('nonactive');
-    //     }
-    //     if(nextButton.disabled === false){
-    //         nextButton.classList.add('active');
-    //         nextButton.classList.remove('nonactive');
-    //     }
-    //     setTimeout(checkBtns, 1000);
-    // }
+        if(prevButton.disabled === true){
+            prevButton.classList.add('nonactive');
+            prevButton.classList.remove('active');
+        }
+        if(nextButton.disabled === true){
+            nextButton.classList.add('nonactive');
+            nextButton.classList.remove('active');
+        }
+        if(prevButton.disabled === false){
+            prevButton.classList.add('active');
+            prevButton.classList.remove('nonactive');
+        }
+        if(nextButton.disabled === false){
+            nextButton.classList.add('active');
+            nextButton.classList.remove('nonactive');
+        }
+        setTimeout(checkBtns, 1000);
+    }
     
-    // function getItemFocus(item){
-    //     for(let i = 0; i < itemsCount; i++){
-    //         if(item == i){
-    //             return items[i]
-    //         }
-    //     }
-    // }
-    //     function addStyle(item){
+    function getItemFocus(item){
+        for(let i = 0; i < itemsCount; i++){
+            if(item == i){
+                return items[i]
+            }
+        }
+    }
+        function addStyle(item){
 
-    //     item.classList.add('active-item');
-    //     }
-    //     function removeStyle(item){
+        item.classList.add('active-item');
+        }
+        function removeStyle(item){
 
-    //         item.classList.remove('active-item');
+            item.classList.remove('active-item');
 
-    //     }
+        }
